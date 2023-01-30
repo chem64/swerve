ae79f2cb3e9ce8eddb0ce079b9173d1b7c5d6f64
chem64 <68355041+chem64@users.noreply.github.com>
2023-01-29 20:07:21 -0600

    Add files via upload

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <units/angular_velocity.h>
 //Step1:  Set wheelbase and wheelwidth
 //Step2:  Set all CAN ID numbers using Phoenix Tuner
 //Step3:  Set each wheel to point forward (use straight-edge)
@@ -8,9 +8,11 @@
 
 namespace constants
 {
-  constexpr bool kSwerveOptimizeAngles = false;
-  constexpr double kSwerveAngleBreak = 90;
-  constexpr bool kSwerveReturnToZero = true;
+  constexpr bool kSwerveOptimizeAngles = true;
+  constexpr double kSwerveAngleBreak = 100;
+  constexpr bool kSwerveReturnToZero = false;
+  constexpr double kSwerveDriveSpeedFilter = 2.0;  //smaller is more filtering (slower to reach top speed) - will bypass filter when joystick released
+
   constexpr double kRadtoDeg = 57.2957795;
   constexpr double kDeg2Rad = 0.017453292519943295;
 
@@ -33,47 +35,52 @@ namespace constants
   constexpr int kRearRightDrive_ID = 20;
   constexpr int kRearRightEncoder_ID = 7;
 
+  constexpr double kEncoderCountsPerDegree = 4096.0 / 360.0;
 
-  constexpr double kTurn_KP = 0.5; 
-  constexpr double kTurn_KI = 0.001;
+  //read these from cancoder absolute position when cancoder is configured to -180 to 180 range
+  constexpr double kFrontRightOffset = 26.9;
+  constexpr double kFrontLeftOffset = 37.0;
+  constexpr double kRearLeftOffset = 66.6;
+  constexpr double kRearRightOffset = 98.7;
+
+  constexpr double kTurn_KP = 0.01; 
+  constexpr double kTurn_KI = 0;
   constexpr double kTurn_KD = 0; 
   constexpr double kTurn_KF = 0;
   constexpr double kTurn_IZ = 20; 
 
-  constexpr double kTurnPeakOutputForward = 0.2;
-  constexpr double kTurnPeakOutputReverse = -0.2;
+  constexpr double kTurnPeakOutputForward = 0.5;
+  constexpr double kTurnPeakOutputReverse = -0.5;
   //Supply Limiting is to prevent breakers tripping or brownouts
   constexpr double kTurnSupplyCurrentLimit = 20.0; //amps
-  constexpr double kTurnPeakCurrentLimit = 30.0; //amps
+  constexpr double kTurnPeakCurrentLimit = 25.0; //amps
   constexpr double kTurnPeakCurrentDuration = 25; //msecs
   //Stator limiting is to limit acceleration or heat
   constexpr double kTurnStatorCurrentLimit = 20.0;
-  constexpr double kTurnStatorPeakCurrentLimit = 30.0; //amps
+  constexpr double kTurnStatorPeakCurrentLimit = 25.0; //amps
   constexpr double kTurnStatorPeakCurrentDuration = 15; //msecs
   constexpr double kTurnVoltageCompSaturation = 12.0;
   constexpr double kTurnClosedLoopRamp = 1.0;
 
-  constexpr double kDrivePeakOutputForward = 0.8;
-  constexpr double kDrivePeakOutputReverse = -0.8;
+  constexpr double kDrive_kF = 0.015;
+  constexpr double kDrive_kP = 1.0;
+  constexpr double kDrive_kI = 0.001;
+  constexpr double kDrive_kD = 0.5;
+  constexpr double kDriveOpenLoopRamp = 1.0;
+  constexpr double kDrivePeakOutputForward = 0.3;
+  constexpr double kDrivePeakOutputReverse = -0.3;
   //Supply Limiting is to prevent breakers tripping or brownouts
-  constexpr double kDriveSupplyCurrentLimit = 35.0; //amps
-  constexpr double kDrivePeakCurrentLimit = 40.0; //amps
+  constexpr double kDriveSupplyCurrentLimit = 30.0; //amps
+  constexpr double kDrivePeakCurrentLimit = 35.0; //amps
   constexpr double kDrivePeakCurrentDuration = 25; //msecs
   //Stator limiting is to limit acceleration or heat
-  constexpr double kDriveStatorCurrentLimit = 30.0;
-  constexpr double kDriveStatorPeakCurrentLimit = 35.0; //amps
+  constexpr double kDriveStatorCurrentLimit = 25.0;
+  constexpr double kDriveStatorPeakCurrentLimit = 30.0; //amps
   constexpr double kDriveStatorPeakCurrentDuration = 15; //msecs
   constexpr double kDriveVoltageCompSaturation = 12.0;
-  constexpr double kDriveOpenLoopRamp = 1.5;
-
-  //read these from cancoder absolute position when cancoder is configured to -180 to 180 range
-  constexpr double kFrontLeftOffset = 37.0;  
-  constexpr double kFrontRightOffset = -24.6;
-  constexpr double kRearLeftOffset = 65.8;
-  constexpr double kRearRightOffset = 93.2;
   
-  constexpr double kEncoderCountsPerDegree = 4096.0 / 360.0;
 
+  
   //auto profile - drive wheel position - convert Feet to position units (encoder count)
   constexpr double kDriveCPR = 2048;
   constexpr double kDriveGearRatio = 6.75;
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -7,14 +7,15 @@
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableInstance.h"
 #include "Constants.h"
+#include "Auto_2.h"
+#include "Auto_16.h"
 #include "AHRS.h"
-#include <pathplanner/lib/PathPlannerTrajectory.h>
-#include <pathplanner/lib/PathPlanner.h>
-#include <pathplanner/lib/PathPoint.h>
-
+#include <frc/controller/PIDController.h>
+#include <frc/filter/SlewRateLimiter.h>
 
 class Robot : public frc::TimedRobot {
  public:
+  
   void RobotInit() override;
   void RobotPeriodic() override;
 
@@ -30,16 +31,25 @@ class Robot : public frc::TimedRobot {
   void TestInit() override;
   void TestPeriodic() override;
 
+  double GetHeading();
+  double CheckWrap(double pos);
   void DriveSwerve(double FWD, double STR, double RCW);
   void ConfigMotors();
+  void RunAuto_1();
+  bool AutoIsRunning();
+  void AutoReset();
+  void InitBuffer(BufferedTrajectoryPointStream *bufstrm, const double profile[][2], int totalCnt, bool reverse);
+  void ZeroDistance();
+  void StopAllDrives();
 
   private:
+
     frc::XboxController m_driveController{0};
-    std::shared_ptr<nt::NetworkTable> ntSwerve;
+    std::shared_ptr<nt::NetworkTable> ntBOSS;
     AHRS *ahrs;
     double HeadingOffset = 0;
     double Heading = 0;
-    boolean SwerveOrientationToField = false;
+    bool SwerveOrientationToField = false;
     
     //CAN Devices
     WPI_TalonFX m_frTurn{constants::kFrontRightTurn_ID};
@@ -57,9 +67,17 @@ class Robot : public frc::TimedRobot {
     WPI_TalonFX m_rrTurn{constants::kRearRightTurn_ID};
     WPI_TalonFX m_rrDrive{constants::kRearRightDrive_ID};
     WPI_CANCoder m_rrEncoder{constants::kRearRightEncoder_ID};
-    
-    
+         
     //SWERVE
+    
+    frc2::PIDController m_TurnPID{constants::kTurn_KP, constants::kTurn_KI, constants::kTurn_KD};
+    frc::SlewRateLimiter<units::scalar> spdFilter{2/1_s};
+    
+    double frFlip = 1.0;
+    double flFlip = 1.0;
+    double rlFlip = 1.0;
+    double rrFlip = 1.0;
+
     double forward;
     double strafe;
     double rotate;
@@ -86,5 +104,23 @@ class Robot : public frc::TimedRobot {
   StatorCurrentLimitConfiguration driveStatorSCLC;
   SupplyCurrentLimitConfiguration turnSCLC;
   StatorCurrentLimitConfiguration turnStatorSCLC; 
+
+  //Auto
+  int AutoState = 0;
+  frc::Timer *AutoTimer;
+  frc::Timer *ModeTimer;
+  units::time::second_t ClockStart;
+  units::time::second_t ClockNow;
+  
+  BufferedTrajectoryPointStream *AutoRev16_LeftBufStrm;
+  BufferedTrajectoryPointStream *AutoRev16_RightBufStrm;
+  BufferedTrajectoryPointStream *AutoFwd2_LeftBufStrm;
+  BufferedTrajectoryPointStream *AutoFwd2_RightBufStrm;
+
+  //dashboard variables
+  bool FMSMatch = false;  //true if attached to FMS
+  bool MatchStart = false;
+  int dAutoSelect = 0;
+  int CurMode = 0;
 
   };
